3b57eba335a7ae1daee48be35e78118b74ee08f7
Leonardo <leo.augustobh13@gmail.com>
2023-06-02 16:23:27 -0300

    refactor create-curso.dto.ts and update main

--- src/cursos/cursos.service.ts
+++ src/cursos/cursos.service.ts
@@ -3,47 +3,53 @@ import { CursoEntity } from './entities/curso.entity';
 
 @Injectable()
 export class CursosService {
+  private cursos: CursoEntity[] = [
+    {
+      id: 1,
+      name: 'Fundamentos do Nextjs',
+      description: 'Fundamentos do Nextjs',
+      tags: ['node.js', 'nextjs', 'javascript', 'frontend'],
+    },
+  ];
 
-    private cursos: CursoEntity[] = [
-        { 
-            id: 1,
-            name: "Fundamentos do Nextjs",
-            description: "Fundamentos do Nextjs",
-            tags: ['node.js', 'nextjs', 'javascript', 'frontend'],
-        }
-      
-    ];
+  findAll() {
+    return this.cursos;
+  }
 
-    findAll(){
-       return this.cursos; 
+  findOne(id: string) {
+    const curso = this.cursos.find(
+      (curso: CursoEntity) => curso.id === Number(id),
+    );
+
+    if (!curso) {
+      throw new HttpException(
+        `Curso ID ${id} não encontrado`,
+        HttpStatus.NOT_FOUND,
+      );
     }
+    return curso;
+  }
 
-    findOne(id: string){
-       const curso = this.cursos.find((curso: CursoEntity) => curso.id === Number(id));
-      
-       if ( !curso ){
-        throw new HttpException(`Curso ID ${id} não encontrado`, HttpStatus.NOT_FOUND);
-       }
-       return curso;
+  create(createCursoDto: any) {
+    this.cursos.push(createCursoDto);
+    return createCursoDto;
+  }
+
+  update(id: string, updateCursoDto: any) {
+    const existeCursos = this.cursos.findIndex(
+      (curso: CursoEntity) => curso.id === Number(id),
+    );
+
+    this.cursos[existeCursos] = updateCursoDto;
+  }
+
+  remove(id: string) {
+    const existeCursos = this.cursos.findIndex(
+      (cursos: CursoEntity) => cursos.id === Number(id),
+    );
+
+    if (existeCursos >= 0) {
+      this.cursos.splice(existeCursos, 1);
     }
-
-    create(createCursoDto: any){
-        this.cursos.push(createCursoDto);
-    }
-
-    update(id: string, updateCursoDto: any){
-        const existeCursos = this.cursos.findIndex((curso: CursoEntity) => curso.id === Number(id));
-
-        this.cursos[existeCursos] = updateCursoDto;
-    
-    }
-
-    remove(id: string){
-        const existeCursos = this.cursos.findIndex((cursos: CursoEntity) => cursos.id === Number(id));
-
-        if(existeCursos >= 0) {
-            this.cursos.splice(existeCursos, 1);
-        }
-    }
-
+  }
 }
